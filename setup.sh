8738c0ed2a086bb7a59262a49f36c0bbb5439f62
Greg Ellison <gregellison@Gregs-MacBook-Pro.local>
2016-08-21 12:30:41 -0700

    updating setup for installs on digital ocean

--- setup.sh
+++ setup.sh
@@ -2,16 +2,23 @@
 sudo -su
 
 # install scipy distro
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 
 # install mongodb
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-get update
+apt-get install -y mongodb-org
 
-pip install ratebeer
+# install pip
+apt-get install python-pip python-dev build-essential
+pip install --upgrade pip
+pip install --upgrade virtualenv
+
+# install ratebeer (& depens) and pymongo
+pip install requests
+pip install beautifulsoup4
+pip install lxml
+git clone https://github.com/alilja/ratebeer
 pip install pymongo
-
-# run mongo daemon
-mongod
