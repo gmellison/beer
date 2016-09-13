6efff3e04f0984169fe8239469074951c673334a
greg ellison <gmhellison@gmail.com>
2016-09-13 01:32:24 +0000

    server side changes to mongo_beers script

--- lib/mongo_beers.py
+++ lib/mongo_beers.py
@@ -8,8 +8,10 @@ db = client['beer_db']
 beer_collection = db['beers']
 
 rb = ratebeer.RateBeer()
+e = 0
+n = 0
 
-for i in range(443552):
+for i in range(121952, 443552):
     try:
         beer = rb.beer('/beer/{0}/'.format(i))
         beer['brewery'] = beer['brewery'].url
@@ -17,11 +19,14 @@ for i in range(443552):
 
     except:
         continue
+        e += 1
+    n += 1
 
     # get sleep time from exponential distribution
-    sleep_time = scipy.stats.expon.rvs() / 2.0
+    sleep_time = scipy.stats.expon.rvs() * .75
 
     # every once in a while, sleep longer (I want to be nice to their servers...).
     if i % 500 == 0:
-        sleep_time = 60 + sleep_time
+	print 'ingested {0} messages, time for a quick nap'.format(n - e)
+        sleep_time = 40 + sleep_time
     time.sleep(sleep_time)
