a93ee5a2d224b75f4fb10eb97de1d4cacdb014dd
greg ellison <gmhellison@gmail.com>
2016-09-21 22:52:01 +0000

    updates to (hopefully) make selenium browsing more robust...

--- lib/get_users.py
+++ lib/get_users.py
@@ -2,14 +2,15 @@ import bs4 as bs
 import requests
 import pymongo
 import selenium.webdriver as webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import time
 from math import ceil
 import scipy.stats
 from pyvirtualdisplay import Display
 
 
-def get_browser():
-    browser = webdriver.Firefox()
+def get_browser(binary):
+    browser = webdriver.Firefox(firefox_binary = binary)
     time.sleep(2)
     return browser
 
@@ -23,20 +24,24 @@ def get_user_reviews(user_i, browser):
 
     # first get number of total beers rated for user i: need this to know how many pages of reviews to scrape (@ 50 reviews/page)
     soup = bs.BeautifulSoup(requests.get(base_url).content)
-    nratings = soup.findAll('div', 'stat-value', 'beer-ratings')[0].text
+    
+    divs = soup.findAll('div', 'stat-value', 'beer-ratings')
+    if len(divs) > 0:
+        nratings = divs[0].text
+    else:
+        return user        
 
     if nratings == 0:
-        return None
+        return user
 
     n_pages = int(ceil(int(nratings) / 50.0))
 
     def page_url(page_i):
         return base_url + 'beer-ratings/{0}/'.format(page_i)
 
-    for i in range(1, n_pages):
+    for i in range(1, n_pages + 1):
         page = page_url(i)
-
-        # wrap all browser stuff in a try except since the
+        # wrap all br wser stuff in a try except since the
         # selenium session dies with no recovery once in a while?
         # go to the statsTable on the rating page
         try:
@@ -44,14 +49,14 @@ def get_user_reviews(user_i, browser):
             table = browser.find_element_by_id('statsWindow').find_element_by_class_name('table')
             rows = table.find_elements_by_class_name('accordion-toggle')
         except:
-            browser = get_browser()
+            browser = get_browser(binary)
             get_user_reviews(user_i, browser)
-
         for row in rows:
             beer_url = row.find_element_by_tag_name('a').get_attribute('href')
             beer_id = beer_url.split('/')[5]
             rating = row.find_element_by_tag_name('b').text
-            user['ratings'].append({'beer': beer_id, 'rating': rating})
+            user['ratings'].append({'beer': int(beer_id), 'rating': float(rating)})
+        time.sleep(0.5 + scipy.stats.expon.rvs(0.5))
     return user
 
 if __name__ == '__main__':
@@ -64,16 +69,22 @@ if __name__ == '__main__':
     user_collection = db['users']
     
     u = 0
-
-    browser = get_browser()
+    binary = FirefoxBinary('/usr/lib/firefox/firefox')
+    browser = get_browser(binary)
      
-    for user_id in range(1, 427405):
+    for user_id in range(154, 427406):
 
         sleep_time = scipy.stats.expon.rvs() * 3.0
         time.sleep(sleep_time)
-        user_reviews = get_user_reviews(user_id, browser)
-
-        user_collection.insert_one(user_reviews)
+        try:
+            user_reviews = get_user_reviews(user_id, browser)
+        except: 
+            time.sleep(10)
+            browser = get_browser(binary)
+            user_reviews = get_user_reviews(user_id, browser)
+          
+        if user_reviews is not None:
+            user_collection.insert_one(user_reviews)
         u += 1
         if u % 500 == 0:
             print 'ingested {0} users'.format(u)
